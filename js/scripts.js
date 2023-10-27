c53b3c4311b4e80351da3988e3275297c2ec91ee
Jonathan Song <jonats10@uci.edu>
2023-10-27 10:34:45 -0700

    add logic to double digit input

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,19 @@ function roboger(num) {
     listNum.push(i);
   }
   modifiedListNum = listNum.map((num) => {
-    if (num === 1) {
-      return "Beep!";
-    } else if (num === 2) {
-      return "Boop";
-    } else if (num === 3) {
-      return "Won't you be my neighbor";
-    } else {
-      return num;
+    const numSplit = num.toString().split('')
+    for (let i = 0; i < numSplit.length; i++) {
+      if (numSplit[i] === "1") {
+        return "Beep!"
+      }else if (numSplit[i] === "2") {
+        return "Boop";
+      } else if (numSplit[i] === "3") {
+        return "Won't you be my neighbor";
+      } else {
+        return parseInt(num);
+      }
     }
+  
   });
   return modifiedListNum;
 }
